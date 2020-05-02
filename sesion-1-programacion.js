6c4934fc21e8effa21f6d5b22646b2a8939f384a
Hector Avalos <gerardo.avalos97@hotmail.com>
2020-05-02 10:19:09 -0500

    Initial commit for 'fundamentos-prog-hgac'

---
+++ sesion-1-programacion.js
@@ -0,0 +1,20 @@
+const holamundo = 'Hola mundo'
+const numero = 10.5
+const boolean = false
+const indefinido = undefined
+const nulo = null
+
+const lista = [1, 2, 3, 4, 5]
+const objeto = {
+    clave1: "valor1",
+    key2: "value2",
+}
+function say_hello_function(nombre) {
+    return `Hola ${nombre}`
+}
+const say_hello_anonymous = function(nombre) {
+    return `Hola ${nombre}`
+}
+const say_hello_arrow = (nombre) => {
+    return `Hola ${nombre}`
+}
