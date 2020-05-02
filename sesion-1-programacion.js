e656e5cd74d551fd03e6212bfddaed87b9065975
Hector Avalos <gerardo.avalos97@hotmail.com>
2020-05-02 10:23:50 -0500

    Added date variable

--- sesion-1-programacion.js
+++ sesion-1-programacion.js
@@ -18,3 +18,16 @@ const say_hello_anonymous = function(nombre) {
 const say_hello_arrow = (nombre) => {
     return `Hola ${nombre}`
 }
+const today = new Date();
+
+console.log(holamundo)
+console.log(numero)
+console.log(boolean)
+console.log(indefinido)
+console.log(nulo)
+console.log(lista)
+console.log(objeto)
+console.log(say_hello_function('Hector'))
+console.log(say_hello_anonymous('Carlos'))
+console.log(say_hello_arrow('Luis'))
+console.log("Hoy es", today)
